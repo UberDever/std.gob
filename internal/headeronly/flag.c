be182c7dff6cac4e78db2515612974449bf506c0
uber <uberdever@mail.ru>
2025-12-10 19:46:53 +0300

    added flags

---
+++ internal/headeronly/flag.c
@@ -0,0 +1,2 @@
+#define FLAG_IMPLEMENTATION
+#include "std.gob/third_party/flag.h/flag.h"
--- nob.c
+++ nob.c
@@ -1,11 +1,12 @@
 // clang-format off
-/// NOB_BUILD_LINUX: cc -D_POSIX_C_SOURCE=200809L --std=c99 -Wall -Wextra -I.. -o nob nob.c internal/headeronly/nob.c internal/headeronly/stb_ds.c internal/headeronly/arena.c internal/nob/rebuild_urself.c internal/util.string/string.c
+/// NOB_BUILD_LINUX: cc -D_POSIX_C_SOURCE=200809L --std=c99 -Wall -Wextra -I.. -o nob nob.c internal/headeronly/nob.c internal/headeronly/stb_ds.c internal/headeronly/flag.c internal/headeronly/arena.c internal/nob/rebuild_urself.c internal/util.string/string.c
 // clang-format on
 
 #include "std.gob/third_party/nob.h/nob.h"
 #include "std.gob/internal/nob/api.h"
 #include "std.gob/internal/util.string/api.h"
 #include "std.gob/third_party/arena-allocator/arena.h"
+#include "std.gob/third_party/flag.h/flag.h"
 #include "std.gob/third_party/stb_ds/stb_ds.h"
 
 char g_project_path[1024] = {0};
@@ -20,7 +21,7 @@ char g_project_path[1024] = {0};
 
 #define ASSERT_LOG(cond, fmt, ...)                                                                 \
   if (!(cond)) {                                                                                   \
-    gob_log(NOB_ERROR, fmt, __VA_ARGS__);                                                          \
+    gob_log(NOB_ERROR, fmt, ##__VA_ARGS__);                                                        \
     NOB_UNREACHABLE("assertion failed");                                                           \
   }
 
@@ -38,31 +39,63 @@ struct command_route_t {
 };
 
 static bool command_help(Arena* arena, const command_desc_t* desc, int argc, char** argv) {
-  nob_log(NOB_INFO, "Available commands:");
+  if (argc != 0) {
+    const char* topic = nob_shift(argv, argc);
+    nob_log(NOB_ERROR, "no help topic for '%s'", topic);
+    return false;
+  }
+  fprintf(stderr, "    The commands are:\n");
   for (size_t i = 0; i < stbds_shlen(desc->routes); ++i) {
     const command_route_t* route = &desc->routes[i];
     Nob_String_View route_key = nob_sv_from_cstr(route->key);
     nob_sv_chop_by_delim(&route_key, ' ');
     if (route_key.count != 0) { continue; }
-    nob_log(NOB_INFO, "  %s", route->key);
+    fprintf(stderr, "        %s\n", route->key);
   }
 
-  nob_log(NOB_INFO, "Available help subcommands:");
+  fprintf(stderr, "    Additional help topics:\n");
   for (size_t i = 0; i < stbds_shlen(desc->routes); ++i) {
     const command_route_t* route = &desc->routes[i];
     Nob_String_View route_key = nob_sv_from_cstr(route->key);
     nob_sv_chop_by_delim(&route_key, ' ');
     if (route_key.count == 0) { continue; }
-    nob_log(NOB_INFO, "  %s", route->key);
+    fprintf(stderr, "        %s\n", route->key);
   }
   return true;
 }
 
 static bool command_build(Arena* arena, const command_desc_t* desc, int argc, char** argv) {
+  char* const* build_dir = flag_str("dir", "build", "Build directory");
+  char* const* flag_target = flag_str("target", NULL, "The target we need to execute command for");
+
+  if (argc == 0) {
+    flag_print_options(stderr);
+    return false;
+  }
+
+  // flag_parse expects argv[0] to be the program name, but the dispatcher stripped it off.
+  int adjusted_argc = argc + 1;
+  char** adjusted_argv = arena_alloc(arena, sizeof(char*) * adjusted_argc);
+  ASSERT_LOG(adjusted_argv != NULL, "failed to allocate argv for build command");
+  adjusted_argv[0] = "nob";
+  for (int i = 0; i < argc; ++i) {
+    adjusted_argv[i + 1] = argv[i];
+  }
+
+  if (!flag_parse(adjusted_argc, adjusted_argv)) {
+    flag_print_error(stderr);
+    return false;
+  }
   return true;
 }
 
 static bool command_help_build(Arena* arena, const command_desc_t* desc, int argc, char** argv) {
+  if (argc != 0) {
+    const char* topic = nob_shift(argv, argc);
+    nob_log(NOB_ERROR, "no help topic for '%s'", topic);
+    return false;
+  }
+
   return true;
 }
 
@@ -120,13 +153,15 @@ int main(int argc, char** argv) {
   }
 
   const char* program = nob_shift(argv, argc);
+  char* help_argv = "help";
   if (argc == 0) {
-    gob_log(NOB_ERROR, "no command specified");
-    ret = 1;
-    goto errdefer;
+    argc = 1;
+    argv = &help_argv;
   }
   opt_str_t command = str_from_cstr(arena, nob_shift(argv, argc));
   while (argc > 0) {
+    const char* arg = argv[0];
+    if (arg != NULL && arg[0] == '-') { break; }
     opt_str_t next =
         str_concat_cstr(arena, str_to_cstr(arena, command.value), " ", nob_shift(argv, argc));
     command = next;
@@ -141,7 +176,11 @@ int main(int argc, char** argv) {
     goto errdefer;
   }
   command_desc_t desc = routes[slot].value;
-  desc.handler(arena, &desc, argc, argv);
+  bool result = desc.handler(arena, &desc, argc, argv);
+  if (!result) {
+    ret = 1;
+    goto errdefer;
+  }
 
 errdefer:
   if (defer1) { arena_destroy(arena); }
